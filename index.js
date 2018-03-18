b7dab8cf11cd1dcc5e6c42db4fb60c131080e842
Mohammed Noori <kmanoorib@gmail.com>
2018-03-18 17:17:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 function getFirstSelector(selector) {
   // My code will go here
+  
   return document.querySelector(selector);
+  
 }
 
 function nestedTarget(){
   // My code will go here
+  
+  var theNest = document.getElementById().querySelectorAll();
+  
+  for (var i = 0; i < theNest.length; i++){
+    
+    theNest[i].innerHTML = ( i + 1 ).toString();
+    
+  }
+  
 }
 
 function increaseRankBy(n){
