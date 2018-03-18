523356997e4f60c4955631191213e4148c7b2323
Mohammed Noori <kmanoorib@gmail.com>
2018-03-18 17:10:42 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function getFirstSelector(selector) {
+  // My code will go here
+}
+
+function nestedTarget(){
+  // My code will go here
+}
+
+function increaseRankBy(n){
+  // My code will go here
+}
+
