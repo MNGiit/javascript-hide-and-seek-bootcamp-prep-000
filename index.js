19543402a57fc14fd40fceae51c9582ec9273cd6
Mohammed Noori <kmanoorib@gmail.com>
2018-03-18 21:13:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nestedTarget(){
 function increaseRankBy(n){
   // My code will go here
   
-  var theRanks = document.querySelector(`ul.ranked-list`);
+  var theRanks = document.querySelector(`ul.ranked-list li`);
   
   for (var i = 0; i < theRanks.length; i++){
     
