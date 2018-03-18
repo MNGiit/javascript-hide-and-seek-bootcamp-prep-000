ea679099d174e33330304852414aa0ca055ad89c
Mohammed Noori <kmanoorib@gmail.com>
2018-03-18 19:23:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,15 +8,7 @@ function getFirstSelector(selector) {
 function nestedTarget(){
   // My code will go here
   
-  var theNest = document.getElementById("nested").querySelectorAll("target");
-  
-  for (var i = 0; i < theNest.length; i++){
-    
-    theNest[i].innerHTML = ( i + 1 ).toString();
-    
-  }
-  
-  return theNest;
+  document.querySelector('#nested .target');
   
 }
 
